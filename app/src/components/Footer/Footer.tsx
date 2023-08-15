7b5d7a193633b1940b4e30337190e930f3c5d62d
Michal Kupczak <kurczak12333@gmail.com>
2023-08-15 23:34:24 +0200

    Added bottom component drawer imitation

--- app/src/components/Footer/Footer.tsx
+++ app/src/components/Footer/Footer.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 import styles from "./Footer.module.scss";
 import { LanguageContext } from "App";
+import BottomWhiteVector from "components/Main/components/BottomWhiteVector/BottomWhiteVector";
 
 const Footer = () => {
   const currentLanguageSheet = React.useContext(LanguageContext);
 
   return (
-    <footer className={styles.footerWrapper} id="footer">
-      <div className={styles.contactUs}>
-        {currentLanguageSheet.footer.contactUs}
-      </div>
-      <div className={styles.description}>
-        {currentLanguageSheet.footer.contactUsDescription}
-      </div>
-      <a href="mailto:kurczak12333@gmail.com" className={styles.contacts}>
-        {currentLanguageSheet.footer.email}: kurczak12333@gmail.com
-      </a>
-      <div className={styles.contacts}>
-        {currentLanguageSheet.footer.phonePolish}: +48 505 695 555
-      </div>
-      <div className={styles.contacts}>
-        {currentLanguageSheet.footer.phoneEnglish}: +48 576 590 697
+    <footer id="footer" style={{ background: "white" }}>
+      <BottomWhiteVector />
+      <div className={styles.footerWrapper}>
+        <div className={styles.contactUs}>
+          {currentLanguageSheet.footer.contactUs}
+        </div>
+        <div className={styles.description}>
+          {currentLanguageSheet.footer.contactUsDescription}
+        </div>
+        <a href="mailto:kurczak12333@gmail.com" className={styles.contacts}>
+          {currentLanguageSheet.footer.email}: kurczak12333@gmail.com
+        </a>
+        <div className={styles.contacts}>
+          {currentLanguageSheet.footer.phonePolish}: +48 505 695 555
+        </div>
+        <div className={styles.contacts}>
+          {currentLanguageSheet.footer.phoneEnglish}: +48 576 590 697
+        </div>
       </div>
     </footer>
   );
---
+++ app/src/components/Main/components/BottomWhiteVector/BottomWhiteVector.tsx
@@ -0,0 +1,45 @@
+import VectorMiddlePart from "images/VectorMiddlePart.svg";
+import styles from "./BottomWhiteVector.module.scss";
+import { useEffect, useState } from "react";
+
+const BottomWhiteVector = () => {
+  const [bigComponentWidth, setBigComponentWidth] = useState(0);
+
+  const logit = () => {
+    if (
+      document.body.scrollHeight - window.scrollY - window.innerHeight <
+      800
+    ) {
+      setBigComponentWidth(Math.ceil((document.body.scrollHeight - window.scrollY - window.innerHeight) / 10));
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", logit);
+    // Remove listener
+    return () => {
+      window.removeEventListener("scroll", logit);
+    };
+  }, []);
+
+  return (
+    <div className={styles.vectorWrapper}>
+      <div
+        className={styles.whiteVectorBigPart}
+        style={{ width: `calc(${100 - bigComponentWidth - 10}% - 184px)` }}
+      ></div>
+      <img
+        src={VectorMiddlePart}
+        alt="Movable hidding vector"
+        width={184}
+        height={104}
+      ></img>
+      <div
+        className={styles.whiteVectorSmallPart}
+        style={{ width: `calc(${bigComponentWidth + 10}%)` }}
+      ></div>
+    </div>
+  );
+};
+
+export default BottomWhiteVector;
